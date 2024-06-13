abccfd7f7dc5096af5f4f0e6089ef0017e62900e
M de Kool <matthijsdekool@gmail.com>
2024-06-13 13:12:53 +0200

    Get blocks from db

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+from sqlalchemy.orm import Session
+
 from app.routers.blocks import blockactions
+from app.db.crud import get_blocks
+from app.util.db_dependency import get_db
 from app.util.secure_url import url_for
 
 
@@ -21,10 +25,11 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def root(request: Request):
+async def root(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
+            "blocks": get_blocks(db)
         }
     )
