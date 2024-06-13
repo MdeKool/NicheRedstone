b94beff8cf630af35fb560bebecbcbeda50def5b
MdeKool <matthijsdekool@gmail.com>
2024-06-14 00:05:28 +0200

    Imported new routers

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 
 from app.routers.blocks import blocks
+from app.routers.misc import placeholders
+from app.routers.roadmap import plans
 from app.util.db_dependency import get_db
 from app.util.secure_url import url_for
 
@@ -13,6 +15,8 @@ app = FastAPI(title="Minecraft WiFi Block", version="0.0.1")
 
 # Add routers
 app.include_router(blocks.router)
+app.include_router(placeholders.router)
+app.include_router(plans.router)
 
 # Add mount points for directories outside of app directory
 app.mount("/js", StaticFiles(directory="js"), name="scripts")
