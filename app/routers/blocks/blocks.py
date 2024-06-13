e3f34a5af2e231b3fda0addbd046278c7a3c640e
M de Kool <matthijsdekool@gmail.com>
2024-06-13 18:13:01 +0200

    Added blocks page and changed block removal behaviour

--- app/routers/blocks/blocks.py
+++ app/routers/blocks/blocks.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session as saSession
 
 from app.util.db_dependency import get_db
 from app.db.schemas import BlockCreate
-from app.db.crud import create_block
+from app.db.crud import create_block, get_blocks, get_block_by_id
 
 
 templates = Jinja2Templates(directory="templates")
@@ -15,12 +15,23 @@ router = APIRouter(
 )
 
 
+@router.get("/")
+async def root(request: Request, db: saSession = Depends(get_db)):
+    return templates.TemplateResponse(
+        "blocks.html",
+        {
+            "request": request,
+            "blocks": get_blocks(db)
+        }
+    )
+
+
 @router.put("/add")
 async def add_block(request: Request, db: saSession = Depends(get_db)):
     block = create_block(
         db,
         block=BlockCreate(id=random.randint(1, 65535)),
-        owner_id=0
+        owner_id=random.choice([_ for _ in range(4)])
     )
     return templates.TemplateResponse(
         "partials/block.html",
@@ -36,9 +47,11 @@ async def add_block(request: Request, db: saSession = Depends(get_db)):
 async def remove_block(request: Request, db: saSession = Depends(get_db)):
     data = await request.json()
     block_id = data["blockId"]
-    if block_id in blocks:
-        print("Removing block with id", block_id)
-        blocks.remove(block_id)
-        # TODO: REMOVE BLOCK WITH BLOCK_ID FROM DB
+
+    block = get_block_by_id(db, block_id)
+
+    if block:
+        db.delete(block)
+        db.commit()
         return True
     return False
