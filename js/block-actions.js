3cbe3987118444029f8ef5157a4e0263902dc531
M de Kool <matthijsdekool@gmail.com>
2024-06-13 18:15:22 +0200

    Finished block removal function and Added block sorting function

--- js/block-actions.js
+++ js/block-actions.js
@@ -11,8 +11,8 @@ async function add_block() {
 }
 
 
-async function remove_block(block_id) {
-    let blockId = document.getElementById("remove-block-id").value;
+async function remove_block(blockId) {
+    // let blockId = document.getElementById("remove-block-id").value;
     fetch("/blocks/remove",
         {
             method: "PUT",
@@ -23,10 +23,39 @@ async function remove_block(block_id) {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            if (data.success) {
-                // document.getElementById("block-{{ id }}").remove();
-                // TODO: REMOVE BLOCK WITH BLOCK_ID FROM BLOCKS
+            if (data) {
+                document.getElementById(`block-${blockId}`).remove();
             }
         });
+}
+
+
+async function change_sort(){
+    const blockList = document.getElementById("blocks");
+    const blocks = Array.from(blockList.children);
+
+    const sortOption = document.getElementById("block-sort").value;
+
+    switch (sortOption) {
+        case "block_id": {
+            blocks.sort((a, b) => {
+                const idA = parseInt(a.id.split("-")[1]);
+                const idB = parseInt(b.id.split("-")[1]);
+                return idA - idB;
+            });
+            break;
+        }
+        case "owner": {
+            blocks.sort((a, b) => {
+                const ownerA = parseInt(a.getAttribute("data-owner"));
+                const ownerB = parseInt(b.getAttribute("data-owner"));
+                return ownerA - ownerB;
+            });
+            break;
+        }
+    }
+
+    blocks.forEach(block => {
+        blockList.appendChild(block);
+    });
 }
