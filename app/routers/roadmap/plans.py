200bc3bdb413aafe84eba97827208c1c8ca628ed
mdekool <s1036700@science.ru.nl>
2024-07-08 22:18:41 +0200

    Added endpoint for updating task status

--- app/routers/roadmap/plans.py
+++ app/routers/roadmap/plans.py
@@ -2,8 +2,13 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
-from app.db.crud import create_task as db_create_task, get_root_tasks, delete_task
-from app.db.schemas import TaskCreate
+from app.db.crud import (
+    create_task as db_create_task,
+    get_root_tasks,
+    delete_task,
+    update_task as db_update_task,
+)
+from app.db.schemas import TaskCreate, TaskUpdate
 from app.util.db_dependency import get_db
 
 router = APIRouter(prefix="/plans")
@@ -52,5 +57,19 @@ async def remove_task(request: Request, db: Session = Depends(get_db)):
     data = await request.json()
     task_id = data["task_id"]
     if delete_task(db, task_id):
+        return {"success": True}
+    return {"success": False}
+
+
+@router.put("/update")
+async def update_task(request: Request, db: Session = Depends(get_db)):
+    data = await request.json()
+    task_id = data["task_id"]
+    task = {
+        "name": data.get("name"),
+        "description": data.get("description"),
+        "status": data.get("status"),
+    }
+    if db_update_task(db, task_id=task_id, update=TaskUpdate(**task)):
         return {"success": True}
     return {"success": False}
